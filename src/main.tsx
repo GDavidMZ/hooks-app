9f2378e378727e0ea1eb351260c0dfca70848a94
Germán David <germandavidmz@gmail.com>
2026-09-18 08:12:28 -0500

    installing shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,10 @@ import { TrafficLight } from './01-useState/TrafficLight'
 import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 import { PokemonPage } from './03-examples/PokemonPage'
 import { FocusScreen } from './04-useRef/FocusScreen'
+import { TasksApp } from './05-useReducer/TaskApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FocusScreen />
+    <TasksApp />
   </StrictMode>,
 )
